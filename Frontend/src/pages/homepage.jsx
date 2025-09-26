84743da797b3446f553c80f1e2f95c21304e969a
kunalambarte17 <ambartekunal17@gmail.com>
2025-09-26 15:00:43 +0530

    Change in Headline

--- Frontend/src/pages/homepage.jsx
+++ Frontend/src/pages/homepage.jsx
@@ -5,7 +5,7 @@ export default function HomePage() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-indigo-500 to-purple-600 text-white">
-      <h1 className="text-3xl font-bold mb-6">Welcome to Standalone Guidance System</h1>
+      <h1 className="text-3xl font-bold mb-6">Welcome to Real Time Obstacle Detection System.</h1>
       <button
         onClick={() => navigate("/cam")}
         className="px-6 py-3 bg-white text-indigo-600 font-semibold rounded-2xl shadow-lg hover:bg-gray-200"
